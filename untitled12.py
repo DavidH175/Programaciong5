26827b217c927773ec10fbda12e4c4664498cc60
DavidH175 <104839599+DavidH175@users.noreply.github.com>
2022-05-10 08:50:47 -0500

    Add files via upload

---
+++ untitled12.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 08:41:06 2022
+
+@author: Alumno
+"""
+
+contar=input("ingrese el numero a contar")
+contar=int(contar)
+contador=1
+while True:
+      print(contador)
+      contador+=1
+      if contador>contar:
+        break
