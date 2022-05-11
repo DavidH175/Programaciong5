a755aad082d2987674ca12c9046f3900acdf4118
DavidH175 <104839599+DavidH175@users.noreply.github.com>
2022-05-11 12:05:54 -0500

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,10 @@
+partida1=int(input("ingrese el puntaje de la primera partida"))
+partida2=int(input("ingrese el puntaje de la segunda partida"))
+partida3=int(input("ingrese el puntaje de la tercer partida"))
+if partida1>= partida2 and partida3>= partida2:
+    resultado=partida1+partida3
+elif partida1>= partida3 and partida2>= partida3:
+    resultado=partida1+partida2
+else:
+    resultado=partida2+partida3
+print("el resultado de las partidas es", resultado)
